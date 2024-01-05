f9d32f1bc93f9d904968d9d4326b5d56edff6201
staticWagomU <to6wa17@gmail.com>
2024-01-05 23:55:20 +0900

    Add support for parsing integer literals

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"github.com/staticWagomU/go-monkey/ast"
 	"github.com/staticWagomU/go-monkey/lexer"
 	"github.com/staticWagomU/go-monkey/token"
+	"strconv"
 )
 
 const (
@@ -37,6 +38,7 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	p.nextToken() // ここだけでは、curTokenがセットされないので、2回呼び出す
 	p.nextToken()
@@ -171,4 +173,18 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+	lit.Value = value
+
+	return lit
 }
